bdb53f719d9c393d17767235402e9f60c047a176
dwightgnjohnson <dwightgnjohnson@gmail.com>
2015-02-18 16:12:07 -0800

    setting up api

---
+++ app/assets/javascripts/store.js
@@ -0,0 +1,7 @@
+// app/assets/javascripts/store.js
+App.Store = DS.Store.extend({});
+App.ApplicationAdapter = DS.ActiveModelAdapter.extend({});
+
+DS.RESTAdapter.reopen({
+  namespace: 'api/v1'
+})
